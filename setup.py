b001571d15c44fedcb15e79d56ffbbf63357c32b
Lev Maximov <lev.maximov@gmail.com>
2022-04-22 14:11:40 +0700

    version tick 0.1.4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.3',
+    version='0.1.4',
 
     description='Imports A.ipynb into B.ipynb',
     long_description=long_description,
@@ -54,6 +54,11 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 
     # What does your project relate to?
@@ -65,13 +70,13 @@ setup(
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-    py_modules=["import_ipynb"],
+    py_modules=['import_ipynb'],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['IPython', "nbformat"],
+    install_requires=['IPython', 'nbformat'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
