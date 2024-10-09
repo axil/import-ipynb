b761a4174eb877549bc9a54fe469ab2388d0b887
Lev Maximov <lev.maximov@gmail.com>
2024-10-09 13:02:29 +0700

    reformatted with black

--- import_ipynb.py
+++ import_ipynb.py
@@ -6,7 +6,8 @@ from importlib.machinery import ModuleSpec
 from nbformat import read
 from IPython.core.interactiveshell import InteractiveShell
 
-__version__ = '0.2'
+__version__ = "0.2"
+
 
 def find_notebook(fullname, path=None):
     """find a notebook, given its fully qualified name and an optional path
@@ -15,9 +16,9 @@ def find_notebook(fullname, path=None):
     and tries turning "Foo_Bar" into "Foo Bar" if Foo_Bar
     does not exist.
     """
-    name = fullname.rsplit('.', 1)[-1]
+    name = fullname.rsplit(".", 1)[-1]
     if not path:
-        path = ['']
+        path = [""]
     for d in path:
         nb_path = os.path.join(d, name + ".ipynb")
         if os.path.isfile(nb_path):
@@ -30,6 +31,7 @@ def find_notebook(fullname, path=None):
 
 class NotebookLoader(object):
     """Module Loader for Jupyter Notebooks"""
+
     def __init__(self, path=None):
         self.shell = InteractiveShell.instance()
         self.path = path
@@ -37,13 +39,13 @@ class NotebookLoader(object):
     def create_module(self, spec):
         """import a notebook as a module"""
         path = find_notebook(spec.name, self.path)
-#        print("importing Jupyter notebook from %s" % path)
+        #        print("importing Jupyter notebook from %s" % path)
         mod = ModuleSpec(name=spec.name, loader=self, origin=path)
         return mod
 
     def exec_module(self, mod):
         # load the notebook object
-        with io.open(mod.origin, 'r', encoding='utf-8') as f:
+        with io.open(mod.origin, "r", encoding="utf-8") as f:
             nb = read(f, 4)
 
         # extra work to ensure that magics that would affect the user_ns
@@ -53,9 +55,11 @@ class NotebookLoader(object):
 
         try:
             for cell in nb.cells:
-                if cell.cell_type == 'code':
+                if cell.cell_type == "code":
                     # transform the input to executable Python
-                    code = self.shell.input_transformer_manager.transform_cell(cell.source)
+                    code = self.shell.input_transformer_manager.transform_cell(
+                        cell.source
+                    )
                     # run the code in themodule
                     exec(code, mod.__dict__)
         finally:
@@ -64,6 +68,7 @@ class NotebookLoader(object):
 
 class NotebookFinder(object):
     """Module finder that locates Jupyter Notebooks"""
+
     def __init__(self):
         self.loaders = {}
 
@@ -71,8 +76,9 @@ class NotebookFinder(object):
         nb_path = find_notebook(fullname, path)
         if not nb_path:
             return
-        return importlib.util.spec_from_loader(fullname, NotebookLoader(path), is_package=False)
+        return importlib.util.spec_from_loader(
+            fullname, NotebookLoader(path), is_package=False
+        )
 
 
 sys.meta_path.append(NotebookFinder())
-
